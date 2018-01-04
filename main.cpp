b84e63928b9b519f6eb07b554197171ff12105d5
Mykyta Bondarenko <nikbond@portaone.com>
2018-01-04 20:47:13 +0200

    Init commit

--- main.cpp
+++ main.cpp
@@ -4,5 +4,13 @@
 using namespace asio;
 
 int main() {
+    static const char* SERVER_IP = "127.0.0.1";
+    static int SERVER_PORT = 1234;
+    io_service ioService;
+    ip::tcp::endpoint serverEndPoint(ip::address::from_string(SERVER_IP), SERVER_PORT);
+    ip::tcp::socket socket(ioService);
+    socket.connect(serverEndPoint);
+    socket.write_some(buffer("Hello"));
+    socket.close();
     return 0;
 }
