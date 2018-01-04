4f9c1a79c314c5953f0530a8f57c7ba62d6451bb
Mykyta Bondarenko <nikbond@portaone.com>
2018-01-04 21:32:47 +0200

    Add ability to listen all messages from server

--- main.cpp
+++ main.cpp
@@ -8,17 +8,21 @@ int main() {
     static const char* SERVER_IP = "127.0.0.1";
     static int SERVER_PORT = 1234;
     const char message[] = "Hello, server";
+    ip::tcp::endpoint clientEndPoint(ip::address::from_string(SERVER_IP), 7779);
+
     io_service ioService;
     ip::tcp::endpoint serverEndPoint(ip::address::from_string(SERVER_IP), SERVER_PORT);
-    ip::tcp::socket socket(ioService);
+    ip::tcp::socket socket(ioService, clientEndPoint);
     socket.connect(serverEndPoint);
     socket.write_some(buffer(message));
     std::cout << "Send " << socket.remote_endpoint() << " >(" << strlen(message) << ") " << message << std::endl;
 
-    const int BUFFER_SIZE = 1024;
-    char data[BUFFER_SIZE] = {0};
-    size_t messageLength = socket.read_some(buffer(data));
-    std::cout << "Echo " << socket.remote_endpoint() << " <(" << strlen(data) << ") " << data << std::endl;
+    while(true) {
+        const int BUFFER_SIZE = 1024;
+        char data[BUFFER_SIZE] = {0};
+        size_t messageLength = socket.read_some(buffer(data));
+        std::cout << "Echo " << socket.remote_endpoint() << " <(" << strlen(data) << ") " << data << std::endl;
+    }
     socket.close();
     return 0;
 }
